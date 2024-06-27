ade81008cbeb7b7776e830733199bc1a3ae9dad9
Alexandra Molnar <alexandra_molnar@mckinsey.com>
2024-06-27 13:25:36 +0200

    file upload

--- test/src/App.tsx
+++ test/src/App.tsx
@@ -2,16 +2,38 @@ import { useEffect, useMemo, useState } from "react";
 import "./App.css";
 import { Table } from "./components/table";
 import { useData } from "./hooks/useData";
+import { useForm } from "react-hook-form";
+
+type FormData = {
+  file: File | null;
+};
 
 function App() {
   const [currentData, setCurrentData] = useState<any>([]);
-  const [selectedFile, setSelectedFile] = useState<File | null>(null);
-  const { getData } = useData();
+  const { getData, uploadData } = useData();
+
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    watch,
+    formState: { errors },
+  } = useForm<FormData>({
+    defaultValues: {
+      file: null,
+    },
+  });
+
+  const selectedFile = watch("file");
 
   useEffect(() => {
-    getData().then((data) => {
-      setCurrentData(data);
-    });
+    getData()
+      .then((data) => {
+        setCurrentData(data.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   }, [getData]);
 
   const headers = useMemo(() => {
@@ -23,22 +45,50 @@ function App() {
       : [];
   }, [currentData]);
 
+  const uploadFile = (formData: FormData) => {
+    uploadData(selectedFile as File)
+      .then((data) => {
+        setCurrentData(data.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
   return (
-    <div className="App">
+    <div className="App px-4">
+      <h2 className="text-3xl sm:text-4xl font-extrabold text-center">
+        Lab results
+      </h2>
       {currentData.length > 0 ? (
-        <Table headers={headers} data={currentData} />
+        <div className="full-width overflow-x-auto overflow-hidden">
+          <Table headers={headers} data={currentData} />
+        </div>
       ) : (
-        <div>
+        <form
+          className="flex flex-col items-start gap-2"
+          onSubmit={handleSubmit(uploadFile)}
+        >
+          <h3>No results found, please upload a file</h3>
           <label>Upload data</label>
           <input
             type="file"
             accept=".txt"
             multiple={false}
-            onChange={(e) => setSelectedFile(e.target.files?.[0] ?? null)}
+            {...register("file", { validate: (value) => value !== null })}
+            onChange={(e) => setValue("file", e.target.files?.[0] ?? null)}
           />
+          {errors.file && (
+            <p className="text-red-700">This field is required</p>
+          )}
           {selectedFile && <p>{selectedFile.name}</p>}
-          <button>Submit</button>
-        </div>
+          <button
+            type="submit"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          >
+            Submit
+          </button>
+        </form>
       )}
     </div>
   );
--- test/src/hooks/useData.ts
+++ test/src/hooks/useData.ts
@@ -5,19 +5,29 @@ export const useData = () => {
   const apiUrl = process.env.REACT_APP_API_URL;
 
   const getData = useCallback(async () => {
-    return axios
-      .get(`${apiUrl}/data`, {
-        responseType: "json",
-      })
-      .then((response) => {
-        return response.data;
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+    return axios.get(`${apiUrl}/data`, {
+      responseType: "json",
+    });
   }, [apiUrl]);
+
+  const uploadData = useCallback(
+    (file: File) => {
+      if (!file) return Promise.reject("No file selected");
+
+      const formData = new FormData();
+      formData.append("file", file, file.name);
+
+      return axios.post(`${apiUrl}/upload`, formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+    },
+    [apiUrl]
+  );
 
   return {
     getData,
+    uploadData,
   };
 };
